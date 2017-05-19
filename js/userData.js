021f019b30127978db4607951c2b18232a80f5e0
grevillopes <grevillopes@gmail.com>
2017-05-19 18:27:55 -0500

    updated html, css and js..missing functionality of user add

--- js/userData.js
+++ js/userData.js
@@ -2,39 +2,57 @@
  * Created by grevil on 5/19/2017.
  */
 
-// console.log("from javaScript");
-//
-   var users = [{srno: "1", firstName: "Grevil", lastName: "Lopes", dob: "11/24/1992"},
-    {srno: "1", firstName: "Grevil", lastName: "Lopes", dob: "11/24/1992"}];
-// var add = document.getElementById('add').onclick = addUser();
-//
-funtion addUser(){
+var firstName;
+var lastName;
+var dob;
+var user = {firstName: firstName, lastName: lastName, dob: dob};
+var allUsers = [user];
 
-    var fruits, text, fLen, i;
-
-    fruits = ["Banana", "Orange", "Apple", "Mango"];
-    fLen = fruits.length;
-    text = "<ul>";
-    for (i = 0; i < fLen; i++) {
-        text += "<li>" + fruits[i] + "</li>";
-    }
-    text += "</ul>";
-    document.getElementById("demo").innerHTML = text;
+function updateUser(){
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.dob = dob;
 };
 
+function updateAllUsers() {
+    allUsers.push(user);
+};
 
-function myCreateFunction() {
-    var table = document.getElementById("userTable");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    cell1.innerHTML = users.srno;
-    cell2.innerHTML = users.firstName;
-    cell3.innerHTML = users.lastName;
-    cell4.innerHTML = users.dob;
+function addUser(){
+    firstName = document.getElementById("firstName");
+    lastName = document.getElementById("lastName");
+    dob = document.getElementById("dob");
+    updateUser();
+    updateAllUsers();
+    hideInputField();
+    updateTable();
+
+};
+
+function updateTable(){
+
+    for (i = 0; i < allUsers.length; i++) {
+        var table = document.getElementById("userTable");
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = allUsers[i].firstName;
+        cell2.innerHTML = allUsers[i].lastName;
+        cell3.innerHTML = allUsers[i].dob;
+    }
+};
+
+function showInputField() {
+    var inputField = document.getElementById("inputField");
+    inputField.style.display = 'block'
 }
+
+function hideInputField() {
+    var inputField = document.getElementById("inputField");
+    inputField.style.display = 'none'
+}
+
 
 // function myDeleteFunction() {
 //     document.getElementById("myTable").deleteRow(0);
