3bb8ececef127b9b498ff774e9042fc053b4cbc5
grevillopes <grevillopes@gmail.com>
2017-05-29 18:04:09 -0500

    added highlight selected row

--- js/userData.js
+++ js/userData.js
@@ -7,9 +7,7 @@ var lastName;
 var dob;
 var rIndex;
 var table = document.getElementById("userTable");
-// var user = {firstName: firstName, lastName: lastName, dob: dob};
-
-// var allUsers = [user];
+var origColor;
 var allUsers = [];
 
 function updateUser() {
@@ -22,22 +20,22 @@ function updateUser() {
 };
 
 function addUser(y) {
-    if(y.value === "Edit User"){
+    if (y.value === "Edit User") {
         editUser();
         hideInputField();
         clearInputField();
         disableButtons();
+        unhighlightRows();
     }
-    else{
-    firstName = document.getElementById("firstName").value;
-    lastName = document.getElementById("lastName").value;
-    dob = document.getElementById("dob").value;
-    updateUser();
-    // updateAllUsers();
-    hideInputField();
-    updateTable();
-    clearInputField();
-    // result();
+    else {
+        firstName = document.getElementById("firstName").value;
+        lastName = document.getElementById("lastName").value;
+        dob = document.getElementById("dob").value;
+        updateUser();
+        hideInputField();
+        updateTable();
+        clearInputField();
+        selectedRowToInput();
     }
 };
 
@@ -48,44 +46,50 @@ function editUser() {
         lname = document.getElementById("lastName").value,
         dob = document.getElementById("dob").value;
     // if(!checkEmptyInput()){
-        table.rows[rIndex].cells[0].innerHTML = fname;
-        table.rows[rIndex].cells[1].innerHTML = lname;
-        table.rows[rIndex].cells[2].innerHTML = dob;
+    table.rows[rIndex].cells[0].innerHTML = fname;
+    table.rows[rIndex].cells[1].innerHTML = lname;
+    table.rows[rIndex].cells[2].innerHTML = dob;
     // }
 };
 
-function selectedRowToInput()
-{
-    for(var i = 1; i < table.rows.length; i++)
-    {
-        table.rows[i].onclick = function()
-        {
+function selectedRowToInput() {
+    for (var i = 1; i < table.rows.length; i++) {
+        table.rows[i].onclick = function () {
+            // if(rIndex !== null){
+            //     if(table.rows[rIndex].style.backgroundColor === 'green'){
+            //         unhighlightRows();
+            //         rIndex = null;
+            //     }
+            // }
+
             // get the selected row index
             rIndex = this.rowIndex;
-            this.style.border = 'solid thin';
+
+
+            origColor = this.style.backgroundColor;
+            //this.style.backgroundColor = 'green';
+            highlightRows();
             document.getElementById("firstName").value = this.cells[0].innerHTML;
             document.getElementById("lastName").value = this.cells[1].innerHTML;
             document.getElementById("dob").value = this.cells[2].innerHTML;
 
             enableButtons();
+
         };
     }
 }
 selectedRowToInput();
 
-function editHtmlTbleSelectedRow()
-{
-    var fname = document.getElementById("fname").value,
-        lname = document.getElementById("lname").value,
-        age = document.getElementById("age").value;
-    if(!checkEmptyInput()){
-        table.rows[rIndex].cells[0].innerHTML = fname;
-        table.rows[rIndex].cells[1].innerHTML = lname;
-        table.rows[rIndex].cells[2].innerHTML = age;
-    }
-}
-
-
+// function editHtmlTbleSelectedRow() {
+//     var fname = document.getElementById("fname").value,
+//         lname = document.getElementById("lname").value,
+//         age = document.getElementById("age").value;
+//     if (!checkEmptyInput()) {
+//         table.rows[rIndex].cells[0].innerHTML = fname;
+//         table.rows[rIndex].cells[1].innerHTML = lname;
+//         table.rows[rIndex].cells[2].innerHTML = age;
+//     }
+// }
 
 
 function updateTable() {
@@ -105,13 +109,19 @@ function updateTable() {
     // }
 };
 
+function deleteUser() {
+    table.deleteRow(rIndex);
+    // clear input text
+    clearInputField();
+    disableButtons()
+}
+
 function showInputField(x) {
-    //var x = this.id;
     var caller = x.id;
-    if(caller === "edit"){
+    if (caller === "edit") {
         document.getElementById("addUser").value = "Edit User";
     }
-    else{
+    else {
         document.getElementById("addUser").value = "Add User";
     }
     var inputField = document.getElementById("inputField");
@@ -123,18 +133,28 @@ function hideInputField() {
     inputField.style.display = 'none';
 }
 
-function clearInputField(){
+function clearInputField() {
     document.getElementById("firstName").value = "";
     document.getElementById("lastName").value = "";
     document.getElementById("dob").value = "";
 }
 
-function enableButtons(){
+function enableButtons() {
     document.getElementById("edit").disabled = false;
     document.getElementById("delete").disabled = false;
 }
 
-function disableButtons(){
+function disableButtons() {
     document.getElementById("edit").disabled = true;
     document.getElementById("delete").disabled = true;
 }
+
+function highlightRows() {
+    table.rows[rIndex].style.backgroundColor = 'orange';
+    //table.rows[rIndex].style.color = 'white';
+}
+
+function unhighlightRows() {
+    table.rows[rIndex].style.backgroundColor = origColor;
+}
+
